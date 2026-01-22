faab725c730d86e83c7eb7dd5fc67a2e34e205cd
MiguelCarrillo1 <154398940+MiguelCarrillo1@users.noreply.github.com>
2026-01-21 23:34:04 -0500

    despliegue

--- config/cors.php
+++ config/cors.php
@@ -8,20 +8,13 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_methods' => ['*'],
+'allowed_origins' => ['*'],  # Temporal, luego especifica tu frontend
+'allowed_origins_patterns' => [],
+'allowed_headers' => ['*'],
+'exposed_headers' => [],
+'max_age' => 0,
+'supports_credentials' => true,
 
 ];
